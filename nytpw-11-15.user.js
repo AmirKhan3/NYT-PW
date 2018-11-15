4613f11711874d7bb2e88281d8165d19a829774c
amirkhan3 <30590470+AmirKhan3@users.noreply.github.com>
2018-11-15 15:21:56 -0800

    Added files via tampermonkey export

---
+++ nytpw-11-15.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         Remove NY Times PW
+// @namespace    https://github.com/AmirKhan3/NYT-PW
+// @version      1
+// @description  Removes the NY Times Pawall
+// @author       thekhansolo
+// @include      /^http(s?)://((www\.)?)nytimes\.com(.*)?/\d\d\d\d/(.+)$/
+// @grant        none
+// ==/UserScript==
+
+function addGlobalStyle(css) {
+    var head, style;
+    head = document.getElementsByTagName('head')[0];a
+    if (!head) { return; }
+    style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = css;
+    head.appendChild(style);
+}
+
+addGlobalStyle('#app > div > div:nth-of-type(2) {display: none;}.css-mcm29f {position: initial;overflow: visible;}');
